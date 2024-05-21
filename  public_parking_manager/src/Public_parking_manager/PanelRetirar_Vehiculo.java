5fdbe7fe23eba186673d6d4e42cb0484e81b70b8
LeXScripterX <alexisvasquez998@proton.me>
2024-05-20 20:10:00 -0500

    Tengo algunos errores sin resorver

---
+++  public_parking_manager/src/Public_parking_manager/PanelRetirar_Vehiculo.java
@@ -0,0 +1,34 @@
+
+package Public_parking_manager;
+public class PanelRetirar_Vehiculo extends javax.swing.JPanel {
+
+    
+    public PanelRetirar_Vehiculo() {
+        /**
+         * Create new for principal 
+         */
+      
+        initComponents();
+        setSize(800,450);
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
+        this.setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 400, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 300, Short.MAX_VALUE)
+        );
+    }// </editor-fold>//GEN-END:initComponents
+
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    // End of variables declaration//GEN-END:variables
+}
